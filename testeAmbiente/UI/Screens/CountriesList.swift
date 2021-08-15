1327c8b68ba49992fbef34329451c6e2c20d92a4
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2021-08-15 20:28:48 -0300

    Incluídos screen e model

---
+++ testeAmbiente/UI/Screens/CountriesList.swift
@@ -0,0 +1,30 @@
+//
+//  CountriesList.swift
+//  testeAmbiente
+//
+//  Created by Roberto Edgar Geiss on 15/08/21.
+//
+
+import SwiftUI
+
+struct CountriesList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+// MARK: - Routing
+
+extension CountriesList
+{
+    struct Routing: Equatable
+    {
+        var countryDetails: String
+    }
+}
+struct CountriesList_Previews: PreviewProvider {
+    static var previews: some View {
+        CountriesList()
+    }
+}
---
+++ testeAmbiente/UI/Screens/CountryDetails.swift
@@ -0,0 +1,20 @@
+//
+//  CountriesDetail.swift
+//  testeAmbiente
+//
+//  Created by Roberto Edgar Geiss on 15/08/21.
+//
+
+import SwiftUI
+
+struct CountryDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CountriesDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        CountryDetails()
+    }
+}
